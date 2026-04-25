f8e8aa1cbaeeb56f53ba236c2dc7501d7cd0d53a
mrghost077 <msnlms86@gmail.com>
2026-04-25 13:01:14 +0530

    Added : Login Request and response model

---
+++ app/src/main/java/com/mrghost077/revenantrx/models/LoginResponse.java
@@ -0,0 +1,19 @@
+package com.mrghost077.revenantrx.models;
+
+public class LoginResponse {
+    private String token;
+    private String message;
+
+    public LoginResponse(String token, String message) {
+        this.token = token;
+        this.message = message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
